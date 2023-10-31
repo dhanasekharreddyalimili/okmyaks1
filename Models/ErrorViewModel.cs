22c5279c1f3fb1ee6fe4a9a620fa587bd45fcdbe
cloudadmin <cloudadmin@vm1>
2023-10-31 02:58:08 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace okmyaks1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
